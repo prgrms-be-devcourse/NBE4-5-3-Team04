dbdad14b1051c16fbefa57f5dddccffb55723a47
SeongCheol Jung <kir5121@naver.com>
2025-03-11 11:41:22 +0900

    [feat] 오류시 모든 오류가 인증 하지 않은 오류로 나타나는 현상 수정

--- backend/src/main/java/com/project2/global/security/SecurityConfig.java
+++ backend/src/main/java/com/project2/global/security/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.project2.global.security;
 
-import com.project2.global.app.AppConfig;
-import com.project2.global.dto.RsData;
-import com.project2.global.util.Ut;
-import lombok.RequiredArgsConstructor;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -17,7 +15,11 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import com.project2.global.app.AppConfig;
+import com.project2.global.dto.RsData;
+import com.project2.global.util.Ut;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -31,59 +33,41 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http
-			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/h2-console/**", "/auth/**", "/oauth2/**", "/v3/api-docs",
-						"/api/members/login", "/api/members/logout", "/login").permitAll()
-				.anyRequest().authenticated() // 모든 요청에 대해 인증 필요하도록 변경
+		http.authorizeHttpRequests(
+				auth -> auth.requestMatchers("/h2-console/**", "/auth/**", "/oauth2/**", "/v3/api-docs",
+						"/api/members/login", "/api/members/logout", "/login", "/error")
+					.permitAll()
+					.anyRequest()
+					.authenticated() // 모든 요청에 대해 인증 필요하도록 변경
 			)
 			.csrf(AbstractHttpConfigurer::disable)
 			.sessionManagement(sessionManagement -> {
 				sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 			})
-			.securityContext(securityContext -> securityContext
-					.securityContextRepository(new NullSecurityContextRepository())
-			)
+			.securityContext(
+				securityContext -> securityContext.securityContextRepository(new NullSecurityContextRepository()))
 			.oauth2Login(oauth2 -> {
 				oauth2.authorizationEndpoint(
-						authorizationEndpoint -> authorizationEndpoint
-								.authorizationRequestResolver(customAuthorizationRequestResolver)
-				);
+					authorizationEndpoint -> authorizationEndpoint.authorizationRequestResolver(
+						customAuthorizationRequestResolver));
 				oauth2.successHandler(customAuthenticationSuccessHandler);
 			})
 			.addFilterBefore(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-			.logout(logout -> logout
-					.logoutUrl("/api/members/logout") // 로그아웃 URL 지정
-					.logoutSuccessHandler(customLogoutSuccessHandler)
-			)
-			.headers((headers) -> headers
-					.addHeaderWriter(new XFrameOptionsHeaderWriter(
-							XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+			.logout(logout -> logout.logoutUrl("/api/members/logout") // 로그아웃 URL 지정
+				.logoutSuccessHandler(customLogoutSuccessHandler))
+			.headers((headers) -> headers.addHeaderWriter(
+				new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
 			.exceptionHandling(
-				exceptionHandling -> exceptionHandling
-						.authenticationEntryPoint(
-								(request, response, authException) -> {
-									response.setContentType("application/json;charset=UTF-8");
-									response.setStatus(401);
-									response.getWriter().write(
-											Ut.Json.toString(
-													new RsData("401-1", "잘못된 인증키입니다.")
-											)
-									);
-								}
-						)
-						.accessDeniedHandler(
-								(request, response, authException) -> {
-									response.setContentType("application/json;charset=UTF-8");
-									response.setStatus(403);
-									response.getWriter().write(
-											Ut.Json.toString(
-													new RsData("403-1", "접근 권한이 없습니다.")
-											)
-									);
-								}
-						)
-				);;
+				exceptionHandling -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+					response.setContentType("application/json;charset=UTF-8");
+					response.setStatus(401);
+					response.getWriter().write(Ut.Json.toString(new RsData("401-1", "잘못된 인증키입니다.")));
+				}).accessDeniedHandler((request, response, authException) -> {
+					response.setContentType("application/json;charset=UTF-8");
+					response.setStatus(403);
+					response.getWriter().write(Ut.Json.toString(new RsData("403-1", "접근 권한이 없습니다.")));
+				}));
+		;
 
 		return http.build();
 	}
