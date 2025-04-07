e3eb9305f15dd0d5a2f9de2ab4f7ed0cf39240e5
theGreatKang98 <srouc159@gmail.com>
2025-04-07 12:06:56 +0900

    [chore] post builder 제거 (임시)

--- backend/src/main/java/com/project2/domain/post/mapper/ToggleMapper.java
+++ backend/src/main/java/com/project2/domain/post/mapper/ToggleMapper.java
@@ -1,19 +1,22 @@
 package com.project2.domain.post.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.project2.domain.member.entity.Member;
 import com.project2.domain.post.entity.Likes;
 import com.project2.domain.post.entity.Post;
 import com.project2.domain.post.entity.Scrap;
-import org.springframework.stereotype.Component;
 
 @Component
 public class ToggleMapper {
 
     public Likes toLikes(Long userId, Long postId) {
+        Post post = new Post();
+        post.setId(postId);
         return Likes.builder()
-                .post(Post.builder().id(postId).build())
-                .member(Member.builder().id(userId).build())
-                .build();
+            .post(post)
+            .member(Member.builder().id(userId).build())
+            .build();
     }
 
     public Scrap toScrap(Long userId, Post post) {
