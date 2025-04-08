c90675837eda6482008fefce23e04911c9aabf2e
theGreatKang98 <srouc159@gmail.com>
2025-04-08 11:05:33 +0900

    [refactor] controller, service, repository, config convert java to kotlin

---
+++ backend/src/main/kotlin/com/project2/domain/chat/repository/ChatRoomRepository.kt
@@ -0,0 +1,30 @@
+package com.project2.domain.chat.repository
+
+import com.project2.domain.chat.entity.ChatRoom
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.util.*
+
+interface ChatRoomRepository : JpaRepository<ChatRoom, UUID> {
+
+    @Query("""
+        SELECT cr.id FROM ChatRoom cr
+        JOIN cr.members m
+        WHERE m.id IN (:myId, :opponentId)
+        GROUP BY cr.id
+        HAVING COUNT(DISTINCT m.id) = 2
+""")
+    fun findChatRoomIdByMemberIds(
+            @Param("myId") myId: Long,
+            @Param("opponentId") opponentId: Long
+    ): Optional<UUID>
+
+    @Query("""
+        SELECT cr FROM ChatRoom cr 
+        JOIN cr.members m 
+        WHERE m.id in :actorId 
+        ORDER BY cr.createdDate DESC
+    """)
+    fun findByMembers_IdOrderByLatestMessage(@Param("actorId") actorId: Long): List<ChatRoom>
+}
