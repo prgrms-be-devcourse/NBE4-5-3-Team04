6775a36c3e4793854b5d25b06fdd75071c5d83e4
cloudmato <cloudmato00@gmail.com>
2025-04-08 12:04:36 +0900

    [refactor] Place DTO kotlin변환

--- backend/src/main/java/com/project2/domain/place/dto/PlaceDTO.kt
+++ backend/src/main/java/com/project2/domain/place/dto/PlaceDTO.kt
@@ -1,14 +1,6 @@
-package com.project2.domain.place.dto;
+package com.project2.domain.place.dto
 
-import lombok.Getter;
-
-@Getter
-public class PlaceDTO {
-	private final String placeName;
-	private final String category;
-
-	public PlaceDTO(String placeName, String category) {
-		this.placeName = placeName;
-		this.category = category;
-	}
-}
+data class PlaceDTO(
+    val placeName: String,
+    val category: String
+)
