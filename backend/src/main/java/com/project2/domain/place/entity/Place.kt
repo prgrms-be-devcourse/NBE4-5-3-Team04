13187b4ef421c4e18cc96d0084fe497b7f1a98d6
cloudmato <cloudmato00@gmail.com>
2025-04-08 12:04:26 +0900

    [refactor] Place 엔티티 kotlin변환

--- backend/src/main/java/com/project2/domain/place/entity/Place.kt
+++ backend/src/main/java/com/project2/domain/place/entity/Place.kt
@@ -1,60 +1,71 @@
-package com.project2.domain.place.entity;
+package com.project2.domain.place.entity
 
-import java.util.ArrayList;
-import java.util.List;
+import com.project2.domain.place.enums.Category
+import com.project2.domain.place.enums.Region
+import com.project2.domain.post.entity.Post
+import com.project2.global.entity.BaseTime
+import jakarta.persistence.*
 
-import com.project2.domain.place.enums.Category;
-import com.project2.domain.place.enums.Region;
-import com.project2.domain.post.entity.Post;
-import com.project2.global.entity.BaseTime;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.experimental.SuperBuilder;
-
-@Getter
-@Setter
-@SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
-public class Place extends BaseTime {
-	@Id
-	@Column(name = "id")
-	public Long id;
+class Place(
+    @Id
+    @Column(name = "id")
+    var id: Long = 0L,
 
-	@Column(nullable = false)
-	public String name;
+    @Column(nullable = false)
+    var name: String = "",
 
-	@Column(nullable = false)
-	public Double latitude;
+    @Column(nullable = false)
+    var latitude: Double = 0.0,
 
-	@Column(nullable = false)
-	public Double longitude;
+    @Column(nullable = false)
+    var longitude: Double = 0.0,
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	public Region region;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var region: Region = Region.ETC,
 
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
-	public Category category;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    var category: Category = Category.ETC,
 
-	@OneToMany(mappedBy = "place", cascade = CascadeType.ALL, orphanRemoval = true)
-	@Builder.Default
-	public List<Post> posts = new ArrayList<>();
+    @OneToMany(mappedBy = "place", cascade = [CascadeType.ALL], orphanRemoval = true)
+    var posts: MutableList<Post> = mutableListOf()
+) : BaseTime() {
 
-	public String getKrCategory() {
-		return category.getKrCategory();
-	}
+    companion object {
+        @JvmStatic
+        fun builder() = PlaceBuilder()
+    }
+
+    class PlaceBuilder {
+        private var id: Long = 0L
+        private var name: String = ""
+        private var latitude: Double = 0.0
+        private var longitude: Double = 0.0
+        private var region: Region = Region.ETC
+        private var category: Category = Category.ETC
+        private var posts: MutableList<Post> = mutableListOf()
+
+        fun id(id: Long) = apply { this.id = id }
+        fun name(name: String) = apply { this.name = name }
+        fun latitude(latitude: Double) = apply { this.latitude = latitude }
+        fun longitude(longitude: Double) = apply { this.longitude = longitude }
+        fun region(region: Region) = apply { this.region = region }
+        fun category(category: Category) = apply { this.category = category }
+        fun posts(posts: MutableList<Post>) = apply { this.posts = posts }
+
+        fun build() = Place(
+            id = id,
+            name = name,
+            latitude = latitude,
+            longitude = longitude,
+            region = region,
+            category = category,
+            posts = posts
+        )
+    }
+
+    val krCategory: String
+        get() = category.krCategory
 }
