3ec92e1122f559508529fb726e66cf2d44fa4b8a
cloudmato <cloudmato00@gmail.com>
2025-04-08 12:16:13 +0900

    [refactor] Place Repository kotlin변환

--- backend/src/main/java/com/project2/domain/place/repository/PlaceRepository.kt
+++ backend/src/main/java/com/project2/domain/place/repository/PlaceRepository.kt
@@ -1,8 +1,6 @@
-package com.project2.domain.place.repository;
+package com.project2.domain.place.repository
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.project2.domain.place.entity.Place
+import org.springframework.data.jpa.repository.JpaRepository
 
-import com.project2.domain.place.entity.Place;
-
-public interface PlaceRepository extends JpaRepository<Place, Long> {
-}
+interface PlaceRepository : JpaRepository<Place, Long>
