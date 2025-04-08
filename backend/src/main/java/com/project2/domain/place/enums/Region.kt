e2c760eb58f953367c2ffafb8af30c419925fb35
cloudmato <cloudmato00@gmail.com>
2025-04-08 12:16:25 +0900

    [refactor] Place 관련 enum들 kotlin변환

--- backend/src/main/java/com/project2/domain/place/enums/Region.kt
+++ backend/src/main/java/com/project2/domain/place/enums/Region.kt
@@ -1,32 +1,20 @@
-package com.project2.domain.place.enums;
+package com.project2.domain.place.enums
 
-import java.util.HashMap;
-import java.util.Map;
+enum class Region(val krRegion: String) {
+    SEOUL("서울"), BUSAN("부산"), DAEGU("대구"), INCHEON("인천"), GWANGJU("광주"), DAEJEON("대전"), ULSAN("울산"), SEJONG("세종특별자치시"), GYEONGGI(
+        "경기"
+    ),
+    GANGWON("강원특별자치도"), CHUNGBUK("충북"), CHUNGNAM("충남"), JEONBUK("전북특별자치도"), JEONNAM("전남"), GYEONGBUK("경북"), GYEONGNAM("경남"), JEJU(
+        "제주특별자치도"
+    ),
+    ETC("기타");
 
-import lombok.Getter;
+    companion object {
+        private val REGION_MAP: Map<String, Region> = entries.associateBy { it.krRegion }
 
-@Getter
-public enum Region {
-	SEOUL("서울"), BUSAN("부산"), DAEGU("대구"), INCHEON("인천"), GWANGJU("광주"), DAEJEON("대전"), ULSAN(
-		"울산"), SEJONG("세종특별자치시"), GYEONGGI("경기"), GANGWON("강원특별자치도"), CHUNGBUK("충북"), CHUNGNAM("충남"), JEONBUK(
-		"전북특별자치도"), JEONNAM("전남"), GYEONGBUK("경북"), GYEONGNAM("경남"), JEJU("제주특별자치도"), ETC("기타");
-
-	public final String krRegion;
-
-	// 지역 한글명을 받아 코드로 변환 해주주는 부분.
-	Region(String krRegion) {
-		this.krRegion = krRegion;
-	}
-
-	private static final Map<String, Region> REGION_MAP = new HashMap<>();
-
-	static {
-		for (Region region : Region.values()) {
-			REGION_MAP.put(region.krRegion, region);
-		}
-	}
-
-	public static Region fromKrRegion(String krRegion) {
-		return REGION_MAP.getOrDefault(krRegion, ETC);
-	}
+        @JvmStatic
+        fun fromKrRegion(krRegion: String): Region {
+            return REGION_MAP[krRegion] ?: ETC
+        }
+    }
 }
