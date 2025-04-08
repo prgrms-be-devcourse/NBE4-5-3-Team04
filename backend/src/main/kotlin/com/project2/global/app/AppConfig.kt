293ec84c711816c6965b746cfd21c4e2aff87236
theGreatKang98 <srouc159@gmail.com>
2025-04-08 12:47:48 +0900

    [refactor] global/app 안에 있는것 convert java to kotlin

---
+++ backend/src/main/kotlin/com/project2/global/app/AppConfig.kt
@@ -0,0 +1,20 @@
+package com.project2.global.app
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AppConfig {
+
+    companion object {
+        @JvmStatic
+        lateinit var objectMapper: ObjectMapper
+            private set
+    }
+
+    @Autowired
+    fun setObjectMapper(objectMapper: ObjectMapper) {
+        AppConfig.objectMapper = objectMapper
+    }
+}
