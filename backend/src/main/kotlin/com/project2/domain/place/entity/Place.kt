225e4d7d3b8eb0f7d5000894ea088b1da292da2d
cloudmato <109732067+cloudmato@users.noreply.github.com>
2025-04-09 17:30:17 +0900

    Refactor/39 place location and entity update (#55)
    * [refactor] kotlin 변환 완료된 Place 관련 파일들 디렉토리 이동(java -> kotlin)
    
    * [refactor] Place 엔티티 필드 nullable + !! 방식 적용
    
    * [refactor] Place 엔티티 수정 관련 파일들 수정
    
    * [refactor] Place 엔티티 필드의 nullable(?)제거
    
    * [test] Place 생성 방식 수정으로 인한 테스트 코드 반영

--- backend/src/main/kotlin/com/project2/domain/place/entity/Place.kt
+++ backend/src/main/kotlin/com/project2/domain/place/entity/Place.kt
@@ -9,17 +9,17 @@ import jakarta.persistence.*
 @Entity
 class Place(
     @Id
-    @Column(name = "id")
-    var id: Long = 0L,
+    @Column(name = "id", nullable = false)
+    var id: Long,
 
     @Column(nullable = false)
-    var name: String = "",
+    var name: String,
 
     @Column(nullable = false)
-    var latitude: Double = 0.0,
+    var latitude: Double,
 
     @Column(nullable = false)
-    var longitude: Double = 0.0,
+    var longitude: Double,
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
@@ -39,27 +39,27 @@ class Place(
     }
 
     class PlaceBuilder {
-        private var id: Long = 0L
-        private var name: String = ""
-        private var latitude: Double = 0.0
-        private var longitude: Double = 0.0
+        private var id: Long? = null
+        private var name: String? = null
+        private var latitude: Double? = null
+        private var longitude: Double? = null
         private var region: Region = Region.ETC
         private var category: Category = Category.ETC
         private var posts: MutableList<Post> = mutableListOf()
 
-        fun id(id: Long) = apply { this.id = id }
-        fun name(name: String) = apply { this.name = name }
-        fun latitude(latitude: Double) = apply { this.latitude = latitude }
-        fun longitude(longitude: Double) = apply { this.longitude = longitude }
+        fun id(id: Long?) = apply { this.id = id }
+        fun name(name: String?) = apply { this.name = name }
+        fun latitude(latitude: Double?) = apply { this.latitude = latitude }
+        fun longitude(longitude: Double?) = apply { this.longitude = longitude }
         fun region(region: Region) = apply { this.region = region }
         fun category(category: Category) = apply { this.category = category }
         fun posts(posts: MutableList<Post>) = apply { this.posts = posts }
 
         fun build() = Place(
-            id = id,
-            name = name,
-            latitude = latitude,
-            longitude = longitude,
+            id = id!!,
+            name = name!!,
+            latitude = latitude!!,
+            longitude = longitude!!,
             region = region,
             category = category,
             posts = posts
@@ -68,4 +68,4 @@ class Place(
 
     val krCategory: String
         get() = category.krCategory
-}
+    }
