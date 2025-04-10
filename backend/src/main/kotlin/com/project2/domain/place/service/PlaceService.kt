ee04feb18ef7049e90df3037df6d00a491fe981b
cloudmato <cloudmato00@gmail.com>
2025-04-10 15:35:35 +0900

    [refactor] Place 엔티티 Builder 제거 및 관련 서비스 코드 리팩토링

--- backend/src/main/kotlin/com/project2/domain/place/service/PlaceService.kt
+++ backend/src/main/kotlin/com/project2/domain/place/service/PlaceService.kt
@@ -1,7 +1,6 @@
 package com.project2.domain.place.service
 
 import com.project2.domain.place.entity.Place
-import com.project2.domain.place.entity.Place.Companion.builder
 import com.project2.domain.place.enums.Category.Companion.fromKrCategory
 import com.project2.domain.place.enums.Region.Companion.fromKrRegion
 import com.project2.domain.place.repository.PlaceRepository
@@ -20,8 +19,14 @@ class PlaceService(
         val categoryEnum = fromKrCategory(category)
 
         return placeRepository.findById(placeId).orElseGet {
-            val newPlace = builder().id(placeId).name(name).latitude(latitude).longitude(longitude).region(regionEnum)
-                .category(categoryEnum).build()
+            val newPlace = Place(
+                id = placeId,
+                name = name,
+                latitude = latitude,
+                longitude = longitude,
+                region = regionEnum,
+                category = categoryEnum
+            )
             placeRepository.save(newPlace)
         }
     }
